c7318035874e6a1f4e880a93d273a9e2f67d15fa
D0hwQ1 <pjt137@naver.com>
2022-09-07 10:10:09 +0900

    Set base files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+require("dotenv").config();
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
