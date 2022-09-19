fd13b2c2ca2801c3036da52d8566a74cf00a22fd
D0hwQ1 <pjt137@naver.com>
2022-09-19 14:16:42 +0900

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
             </Box>
             <p style={{ wordBreak: "keep-all" }}>해당 프로젝트는 블록체인 네트워크에 불법 사이트나 범죄자, 스팸을 제보하여 등록하는 시스템입니다.</p>
             <p style={{ wordBreak: "keep-all" }}>활동하시면, 3시간마다 활동 수가 1씩 증가됩니다.</p>
-            <p style={{ wordBreak: "keep-all" }}>활동은 총 [리포트 작성/댓글 달기/리포트 링크 수정]이 있습니다.</p>
+            <p style={{ wordBreak: "keep-all" }}>활동은 총 [리포트 작성/리포트 추천/광고 신청] 등이 있습니다.</p>
             <div style={{ height: "10vh" }}></div>
             <p style={{ wordBreak: "keep-all" }}>블록체인은 Golang을 사용하여 개발했으며, Docker로 배포가 되었습니다.</p>
             <p style={{ wordBreak: "keep-all" }}>프론트엔드는 Next.js(React)를 사용하여 개발했으며, 이 또한 Docker로 배포가 되었습니다.</p>
